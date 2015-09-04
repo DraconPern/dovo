0dc807b0e366ad02f82cc75b0e79126837719439
DraconPern <draconpern@hotmail.com>
2015-09-04 17:02:40 -0500

    sort instance by natural order

--- src/dovo_mainFrame.cpp
+++ src/dovo_mainFrame.cpp
@@ -1,5 +1,7 @@
 #include "dovo_mainFrame.h"
 #include <boost/lexical_cast.hpp>
+#include <map>
+#include "alphanum.hpp"
 
 dovo_mainFrame::dovo_mainFrame( wxWindow* parent )
 	:
@@ -38,7 +40,7 @@ dovo_mainFrame::dovo_mainFrame( wxWindow* parent )
 
 	FillDestinationList();
 
-	image.Create(1, 1);	
+	image.Create(1, 1);
 }
 
 void dovo_mainFrame::OnBrowse( wxCommandEvent& event )
@@ -97,14 +99,27 @@ void dovo_mainFrame::OnStudiesSelected( wxListEvent& event )
 		m_series->SetItemState(0, wxLIST_STATE_SELECTED, wxLIST_STATE_SELECTED);
 }
 
+typedef std::map<std::string, std::string, doj::alphanum_less<std::string> > naturalmap;
+
 void dovo_mainFrame::OnSeriesSelected( wxListEvent& event )
 {
 	long item = m_series->GetNextItem(-1, wxLIST_NEXT_ALL, wxLIST_STATE_SELECTED);
 	if (item == -1)
 		return;
 
+	naturalmap entries;
+
 	m_instances->DeleteAllItems();
-	m_engine.GetInstances(m_series->GetItemText(item, 1).ToUTF8().data(), fillinstances, this);
+	m_engine.GetInstances(m_series->GetItemText(item, 1).ToUTF8().data(), fillinstances, &entries);
+
+	int j = 0;
+	for(naturalmap::iterator ii = entries.begin(); ii != entries.end(); ++ii)
+	{
+		m_instances->InsertItem(j, wxString::FromUTF8((*ii).first.c_str()));
+		m_instances->SetItem(j, 1, wxString::FromUTF8((*ii).second.c_str()));
+		j++;
+	}
+
 	m_instances->SetColumnWidth(0, wxLIST_AUTOSIZE);
 	m_instances->SetColumnWidth(1, wxLIST_AUTOSIZE);
 
@@ -115,7 +130,7 @@ void dovo_mainFrame::OnSeriesSelected( wxListEvent& event )
 void dovo_mainFrame::OnPaintPreview( wxPaintEvent& event )
 {
 	wxPaintDC dc(m_preview);
-	renderPreview(dc);	
+	renderPreview(dc);
 }
 
 void dovo_mainFrame::OnInstancesSelected( wxListEvent& event )
@@ -131,7 +146,7 @@ void dovo_mainFrame::OnInstancesSelected( wxListEvent& event )
 #endif
 
 	wxSize s = m_preview->GetClientSize();
-	
+
 	wxBusyCursor wait;
 	dcm2img(filename, s.GetWidth(), s.GetHeight(), image);
 
@@ -187,9 +202,9 @@ void dovo_mainFrame::OnSend( wxCommandEvent& event )
 	wxString patientid = m_patients->GetItemText(item, 1);
 	wxString birthday = m_patients->GetItemText(item, 2);
 	dovo_changePatientInfo changepatinfo(this);
-	changepatinfo.m_patientName = m_patients->GetItemText(item);	
+	changepatinfo.m_patientName = m_patients->GetItemText(item);
 	changepatinfo.m_patientID = m_patients->GetItemText(item, 1);
-	changepatinfo.m_birthday = m_patients->GetItemText(item, 2);	
+	changepatinfo.m_birthday = m_patients->GetItemText(item, 2);
 
 	wxString new_patientName, new_patientID, new_birthday;
 	if(changepatinfo.ShowModal() == wxID_OK)
@@ -229,17 +244,17 @@ void dovo_mainFrame::FillDestinationList()
 {
 	// add to combo box
 	m_destination->Clear();
-	std::vector<DestinationEntry>::iterator itr;	
-	for(itr = m_engine.globalDestinations.begin(); itr != m_engine.globalDestinations.end(); itr++)	
+	std::vector<DestinationEntry>::iterator itr;
+	for(itr = m_engine.globalDestinations.begin(); itr != m_engine.globalDestinations.end(); itr++)
 		m_destination->Append(wxString::FromUTF8((*itr).name.c_str()) + L" (*)");
 
 	for(itr = m_engine.destinations.begin(); itr != m_engine.destinations.end(); itr++)
-		m_destination->Append(wxString::FromUTF8((*itr).name.c_str()));	
+		m_destination->Append(wxString::FromUTF8((*itr).name.c_str()));
 }
 
 int dovo_mainFrame::fillstudies(void *param,int columns,char** values, char**names)
 {
-	dovo_mainFrame *win = (dovo_mainFrame *) param;	
+	dovo_mainFrame *win = (dovo_mainFrame *) param;
 
 	std::string date;
 	date = values[2];
@@ -267,31 +282,31 @@ int dovo_mainFrame::fillstudies(void *param,int columns,char** values, char**nam
 	win->m_studies->InsertItem(0, buf);
 	win->m_studies->SetItem(0, 1, wxString::FromUTF8(values[1]));
 	win->m_studies->SetItem(0, 2, wxString::FromUTF8(values[0]));
-	return 0; 
+	return 0;
 }
 
 dovo_mainFrame::~dovo_mainFrame()
 {
-	wxConfig::Get()->SetPath("/Settings");	
+	wxConfig::Get()->SetPath("/Settings");
 	wxConfig::Get()->Write("LastDir", m_directory->GetValue());
 	wxConfig::Get()->Flush();
 }
 
 int dovo_mainFrame::fillpatients(void *param,int columns,char** values, char**names)
 {
-	dovo_mainFrame *win = (dovo_mainFrame *) param;	
+	dovo_mainFrame *win = (dovo_mainFrame *) param;
 	win->m_patients->InsertItem(0, values[0]);
 	win->m_patients->SetItem(0, 1, values[1]);
 	win->m_patients->SetItem(0, 2, values[2]);
-	return 0; 
+	return 0;
 }
 
 int dovo_mainFrame::fillseries(void *param,int columns,char** values, char**names)
 {
-	dovo_mainFrame *win = (dovo_mainFrame *) param;	
+	dovo_mainFrame *win = (dovo_mainFrame *) param;
 	win->m_series->InsertItem(0, wxString::FromUTF8(values[1]));
-	win->m_series->SetItem(0, 1, wxString::FromUTF8(values[0]));	
-	return 0; 
+	win->m_series->SetItem(0, 1, wxString::FromUTF8(values[0]));
+	return 0;
 }
 
 void dovo_mainFrame::renderPreview(wxDC& dc)
@@ -301,9 +316,7 @@ void dovo_mainFrame::renderPreview(wxDC& dc)
 
 int dovo_mainFrame::fillinstances(void *param,int columns,char** values, char**names)
 {
-
-	dovo_mainFrame *win = (dovo_mainFrame *) param;	
-	win->m_instances->InsertItem(0, wxString::FromUTF8(values[0]));
-	win->m_instances->SetItem(0, 1, wxString::FromUTF8(values[1]));	
-	return 0; 
+	naturalmap *entries = (naturalmap *)param;
+	entries->insert(std::pair<std::string, std::string>(values[0],values[1]));
+	return 0;
 }
