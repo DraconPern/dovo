38ed4eea1d62d09a2a086a1dc3f70c20921d643c
DraconPern <draconpern@hotmail.com>
2016-04-01 06:17:52 -0500

    fix

--- build-osx.sh
+++ build-osx.sh
@@ -11,7 +11,7 @@ fi
 BUILD_DIR=`pwd`
 DEVSPACE=`pwd`
 
-build.sh $1
+./build.sh $1
 
 cd $BUILD_DIR/build-$TYPE
 hdiutil create -volname dovo -srcfolder $BUILD_DIR/build-$TYPE/dovo.app -ov -format UDZO dovo.dmg
