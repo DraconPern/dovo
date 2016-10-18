9a1904fbe31d416859c04c84b1dc1b4224ac1ad7
DraconPern <draconpern@hotmail.com>
2016-10-18 18:51:36 -0500

    fix race condition

--- src/main.cpp
+++ src/main.cpp
@@ -57,6 +57,8 @@ BEGIN_EVENT_TABLE(MyApp, wxApp)
 		return true;
 	}
 
+	boost::filesystem::path::codecvt();  // ensure VC++ does not race during initialization.
+
 	// check for update	in background and save result for next run
 	updater = boost::thread(&updateChecker);
 
