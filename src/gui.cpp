4004e3e307c9eb5b8510e838d2a6a91034e827de
DraconPern <draconpern@hotmail.com>
2018-02-23 23:36:05 -0600

    updated copyright and url

--- src/gui.cpp
+++ src/gui.cpp
@@ -373,7 +373,7 @@ about::about( wxWindow* parent, wxWindowID id, const wxString& title, const wxPo
 	m_buildinfo->Wrap( -1 );
 	bSizer28->Add( m_buildinfo, 0, wxALL, 5 );
 	
-	m_copyright = new wxStaticText( this, wxID_ANY, _("Copyright (C) 2007-2017 Ing-Long Eric Kuo"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_copyright = new wxStaticText( this, wxID_ANY, _("Copyright (C) 2007-2018 Ing-Long Eric Kuo"), wxDefaultPosition, wxDefaultSize, 0 );
 	m_copyright->Wrap( -1 );
 	bSizer28->Add( m_copyright, 0, wxALL, 5 );
 	
@@ -384,7 +384,7 @@ about::about( wxWindow* parent, wxWindowID id, const wxString& title, const wxPo
 	m_staticText13->Wrap( -1 );
 	bSizer27->Add( m_staticText13, 0, wxALL, 5 );
 	
-	m_hyperlink1 = new wxHyperlinkCtrl( this, wxID_ANY, wxEmptyString, wxT("http://www.draconpern.com/software/dovo"), wxDefaultPosition, wxDefaultSize, wxHL_DEFAULT_STYLE );
+	m_hyperlink1 = new wxHyperlinkCtrl( this, wxID_ANY, wxEmptyString, wxT("https://www.draconpern.com/software/dovo"), wxDefaultPosition, wxDefaultSize, wxHL_DEFAULT_STYLE );
 	bSizer27->Add( m_hyperlink1, 0, wxALL, 5 );
 	
 	
