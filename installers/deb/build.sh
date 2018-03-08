225c677a58b3abdfae5b8e85e985e09af6a3e4a0
DraconPern <draconpern@hotmail.com>
2018-03-08 06:01:31 -0600

    fix deb

---
+++ installers/deb/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -xe
+
+if [ "$1" == "Release" ]
+then
+  TYPE=Release
+else
+  TYPE=Debug
+fi
+
+mkdir -p container/DEBIAN
+cp control container/DEBIAN
+mkdir -p container/usr/bin
+cp build-$TYPE/dovo /usr/bin
+dpkg-deb --build container
