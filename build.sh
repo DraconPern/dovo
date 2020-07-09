786b44294c45e883f174fee18b5d612508fbdccd
DraconPern <draconpern@hotmail.com>
2020-07-09 05:20:46 -0500

    dev

--- build.sh
+++ build.sh
@@ -46,7 +46,7 @@ cd $DEVSPACE
 unzip -n boost_1_63_0.zip
 cd boost_1_63_0
 ./bootstrap.sh
-COMMONb2Flag="-j 4 link=static runtime-link=static stage"
+COMMONb2Flag="-j 4 link=static runtime-link=static address-model=64 architecture=x86 stage"
 BOOSTModule="--with-locale --with-thread --with-filesystem --with-system --with-date_time --with-regex"
 if [ "$TYPE" = "Release" ] ; then
   ./b2 $COMMONb2Flag $BOOSTModule variant=release
