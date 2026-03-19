a3542696f072f8b702a17c1ccc47eb1487df8053
DraconPern <draconpern@hotmail.com>
2026-03-19 05:40:31 -0500

    fix

--- build.sh
+++ build.sh
@@ -77,6 +77,8 @@ fi
 cd $DEVSPACE
 [[ -d wxWidgets ]] || git clone --branch=v3.3.2 --recurse-submodules $githuburl/wxWidgets/wxWidgets.git
 cd wxWidgets
+mkdir -p build-$TYPE
+cd build-$TYPE
 cmake .. -DwxBUILD_SHARED=OFF -DCMAKE_BUILD_TYPE=$TYPE -DCMAKE_INSTALL_PREFIX=$DEVSPACE/wxWidgets/$TYPE
 make -j8 install
 
