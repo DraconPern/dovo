3058bdc4c415141947e1a8ae7da7b9a3eec4a7af
DraconPern <draconpern@hotmail.com>
2026-03-29 00:27:11 -0500

    add

--- installers/flatpak/build.sh
+++ installers/flatpak/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -xe
-flatpak-builder --force-clean --user --install-deps-from=flathub --repo=repo --install builddir com.draconpern.dovo.yml
+#flatpak-builder --force-clean --user --install-deps-from=flathub --repo=repo --install builddir com.draconpern.dovo.yml
+flatpak-builder --force-clean --disable-updates --user --install-deps-from=flathub --repo=repo --install builddir com.draconpern.dovo.yml
 
